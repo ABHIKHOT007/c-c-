cff23ac61e6463427ea39e4aa02e11b019b70a1a
abhi <abhishekanilkhot@gmail.com>
2021-01-01 23:25:00 +0530

    second commit

---
+++ main2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"enter the value of b:"<<endl;
+    cin>>b;
+
+    swap(a,b);
+
+    cout<<"value of the a after swapping is:"<<a<<endl;
+    cout<<endl;
+    cout<<"valueof the b after swapping is:"<<b<<endl;
+
+    return 0;
+}
